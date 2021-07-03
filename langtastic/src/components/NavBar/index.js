220e9232effdacdb78df55337ba13854d006e140
Toby Smith <toby.d.smith@outlook.com>
2021-07-03 15:32:41 +0100

    All components, just css needed

--- langtastic/src/components/NavBar/index.js
+++ langtastic/src/components/NavBar/index.js
@@ -1,11 +1,17 @@
 import Logo from "./Logo";
 import HandburgerMenu from "./HamburgerMenu";
+import Title from "./Title";
+import "./navBar.css";
 
 function NavBar() {
   return (
     <>
       <Logo></Logo>
       <HandburgerMenu></HandburgerMenu>
+      <Title title="Langtastic"></Title>
+      <div className="handburgerbutton"></div>
+      <div className="handburgerbutton"></div>
+      <div className="handburgerbutton"></div>
     </>
   );
 }
