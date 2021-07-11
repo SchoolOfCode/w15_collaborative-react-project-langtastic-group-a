1ef02243a2ee06c03aeed66766692866d02ca135
m-fidow <muna.a.fidow@gmail.com>
2021-07-11 17:37:02 +0100

    created click listener and handle click function for buttons

--- langtastic/src/components/Appointment_Confirmation/Confirmationbox.js
+++ langtastic/src/components/Appointment_Confirmation/Confirmationbox.js
@@ -1,11 +1,16 @@
-import Bookingbutton from "../Tutorselection/Booknowbutton";
-
+import { useState } from "react";
 export default function ConfirmationBox() {
+  const [confirmation, setConfirmation] = useState(null);
+  function handleClick(value) {
+    if (value === "no") {
+      console.log("hello from no");
+    }
+  }
   return (
     <div>
       <p>Are you sure you want to book the session?</p>
-      <Bookingbutton>No</Bookingbutton>
-      <Bookingbutton>Yes</Bookingbutton>
+      <button onClick={() => handleClick("no")}>No</button>
+      <button onClick={() => handleClick("yes")}>Yes</button>
     </div>
   );
 }
