b9baa20f57ac2a8c16fa8a253399bbffb2b93d43
Lewis Murray <lewisdmurray00@gmail.com>
2021-07-25 15:24:09 +0100

    Added a reducer function to the confirmationbox.js file, replacing the useEffect

--- langtastic/src/components/Appointment_Confirmation/Confirmationbox.js
+++ langtastic/src/components/Appointment_Confirmation/Confirmationbox.js
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useReducer } from "react";
+//initial state is false
+//if action is "yes" change state to true and show the confirmed info
+//if action is "no" keep initial state and return to tutor selection.
+const initialState = { confirmed: false };
+function reducer(state, action) {
+  switch (action.type) {
+    case "yes":
+      return { confirmed: true };
+    case "no":
+      return { confirmed: false };
+    default:
+      throw new Error();
+  }
+}
 function ConfirmationBox(props) {
-  const [confirmed, setConfirmed] = useState(false);
-
+  const [state, dispatch] = useReducer(reducer, initialState);
   let box;
-  if (confirmed == true) {
+  if (state.confirmed === true) {
     box = (
       <div>
         <p>Session booked</p>
@@ -13,15 +26,28 @@ function ConfirmationBox(props) {
     );
   } else {
     box = (
-      <div >
+      <div>
         <p>Are you sure you want to book the session?</p>
         <Link to="/tutorselection">
-          <button>No</button>
+          <button
+          // onClick={() => {
+          //   dispatch({ type: "no" });
+          // }}
+          >
+            No+-
+          </button>
         </Link>
-        <button onClick={() => setConfirmed(true)}>Yes</button>
+        <button
+          data-testid="button"
+          onClick={() => {
+            dispatch({ type: "yes" });
+          }}
+        >
+          Yes
+        </button>
       </div>
     );
   }
-  return <div data-testid='confirmationbox-container'>{box}</div>;
+  return <div data-testid="confirmation-box">{box}</div>;
 }
 export default ConfirmationBox;
